2dc36ed0f65befc0a2102144b3496911135e2b3e
RF Calculator <xabc66412@gmail.com>
2025-12-25 23:04:55 +0600

    Refactor service worker event listeners

--- service-worker.js/service-worker.js
+++ service-worker.js/service-worker.js
@@ -1,15 +1,9 @@
 const CACHE="rf-calc-v1";
-self.addEventListener("install",e=>{
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json"
-    ]))
+    caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))
   );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
